1ea3ef70e3fb11acfcc002a8408e1090e0595079
Timo Denk <simssosdenk@googlemail.com>
2015-12-16 14:55:17 +0100

    IP address tool and links between bin to dec and dec to bin pages

---
+++ page/content/ip-address.php
@@ -0,0 +1,9 @@
+<h1>IP address</h1>
+<h2><?php echo $_SERVER['REMOTE_ADDR']; ?></h2>
+
+<div class="text-info">
+	<h3>IP address</h3>
+	<p>
+		An Internet Protocol address (IP address) is a numerical label assigned to each device (e.g., computer, printer) participating in a computer network that uses the Internet Protocol for communication. An IP address serves two principal functions: host or network interface identification and location addressing. Its role has been characterized as follows: "A name indicates what we seek. An address indicates where it is. A route indicates how to get there." <a href="https://en.wikipedia.org/wiki/IP_address" target="_blank">Read more</a>
+	</p>
+</div>
