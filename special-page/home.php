9424ebe73850b2af936b0bc3a33e5b484388ade9
Timo Denk <simssosdenk@googlemail.com>
2017-09-29 21:22:40 +0200

    Update tool descriptions

--- special-page/home.php
+++ special-page/home.php
@@ -1,12 +1,14 @@
 <h1>Home</h1>
 <p class="lead">tools.timodenk.com is a collection of online tools.</p>
 
-<h3>All tools</h3>
+<?php
+	$files = scandir('page/content');
+?>
+<h3>All tools (<?php echo count($files); ?>)</h3>
 <div class="detailed-list-of-tools">
-	<?php
-		$files = scandir('page/content');
+	<?
 		foreach ($files as $key => $value) {
-			$name = substr($value, 0, -4);
+			$name = substr($value, 0, -4); // remove file ending
 			if (strlen($name) > 0) {
 				$meta_data = get_meta_data($name);
 				echo '
