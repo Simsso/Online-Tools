cd9df22becb4e263f7443e1fc49b67a83d8c1e32
Timo Denk <github@timodenk.com>
2020-02-27 10:57:04 +0100

    Write adapter

--- src/page/adapter/csv-column-completion.js
+++ src/page/adapter/csv-column-completion.js
@@ -1,1 +1,23 @@
 const sampleCSV = 'A,B,C\n,,\n,B,D\n"a,a","b",';
+
+(() => {
+    const csvInputTextarea = $('#csv-input'),
+        csvOutputDiv = $('#csv-output'),
+        completeBtn = $('#complete-btn'),
+        showExampleBtn = $('#show-example-btn');
+
+    function showExample() {
+        csvInputTextarea.val(sampleCSV);
+        complete();
+    }
+
+    function complete() {
+        const csvInput = csvInputTextarea.val();
+        const data = parseCSV(csvInput);
+        completeColumns(data);
+        csvOutputDiv.html(exportCSV(data));
+    }
+    
+    showExampleBtn.on('click', showExample);
+    completeBtn.on('click', complete);
+})();
